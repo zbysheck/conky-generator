bdc615fde2c5d678c902b8a35410a70608a60ea0
Kudrys <ryskuduk@gmail.com>
2016-06-06 13:05:57 +0200

    if true false

--- generator.sh
+++ generator.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 touch ~/.conkyrc
-"" > ~/.conkyrc
+echo "" > ~/.conkyrc
 
 ans=$(zenity  --list  --text "Conky Generator" --checklist  --column "Pick" --column "options" FALSE "Zużycie procesora" FALSE "Dostępna pamięć na dysku" FALSE "Zajęta pamięć" FALSE "Zużycie rdzeni procesora" FALSE "Szybkość rdzeni" FALSE "Obciążenie rdzeni" FALSE "Ping" FALSE "Upload" FALSE "Download" FALSE "Obciążenie otwartych aplikacji" FALSE "Dzisiejsza data" FALSE "Aktualna godzina" FALSE "Ilość partycji" FALSE "Obciążenie parycji" FALSE "Stopien naładowania baterii" --separator=":");
-echo $ans
+echo $ansKud
+
+# Sklejanie linijek :
+
+if  true 
+	then
+		echo "prawdagg" >> ~/.conkyrc
+fi
+
+if  false  
+	then
+		echo "nopegg" >> ~/.conkyrc
+fi
