37c7a29d3a8da89ebb6d12a2d26aa0f0c8e977b5
Kudrys <ryskuduk@gmail.com>
2016-06-06 12:48:25 +0200

    dodany touch

--- generator.sh
+++ generator.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+touch ~/.conkyrc
+"" > ~/.conkyrc
+
 ans=$(zenity  --list  --text "Conky Generator" --checklist  --column "Pick" --column "options" FALSE "Zużycie procesora" FALSE "Dostępna pamięć na dysku" FALSE "Zajęta pamięć" FALSE "Zużycie rdzeni procesora" FALSE "Szybkość rdzeni" FALSE "Obciążenie rdzeni" FALSE "Ping" FALSE "Upload" FALSE "Download" FALSE "Obciążenie otwartych aplikacji" FALSE "Dzisiejsza data" FALSE "Aktualna godzina" FALSE "Ilość partycji" FALSE "Obciążenie parycji" FALSE "Stopien naładowania baterii" --separator=":");
 echo $ans
